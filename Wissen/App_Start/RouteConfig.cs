58803fbcdd150f3e384e694855d60839f3a02d18
Murat Demirci <mdemirci@outlook.com>
2019-02-13 11:28:24 +0300

    first version

--- Wissen/App_Start/RouteConfig.cs
+++ Wissen/App_Start/RouteConfig.cs
@@ -12,7 +12,8 @@ namespace Wissen
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(name: "About", url: "hakkimizda",
+                defaults: new { controller = "Home", action = "About" });
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
