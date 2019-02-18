2de9fbec1c51891acaf88014cf0e4dff95dfa917
Murat Demirci <mdemirci@outlook.com>
2019-02-18 11:25:20 +0300

    CategoryController oluşturuluyor

---
+++ Wissen.Admin/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wissen.Model;
+using Wissen.Service;
+
+namespace Wissen.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+        // GET: Category
+        public ActionResult Index()
+        {
+            var categories = categoryService.GetAll();
+            return View(categories);
+        }
+
+        public ActionResult Create()
+        {
+            var category = new Category();
+            return View(category);
+        }
+    }
+}
